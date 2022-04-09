ab332fd9b63b94f6bda28e04f62a6711365dafcc
stoopwaffel <stoopwaffel@gmail.com>
2022-04-08 23:59:13 -0400

    pre stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = 'Blog Post';
+    const body = 'whatup yeehaw this my blog ppl';
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 1, text: 'Comment two'},
+        {id: 1, text: 'Comment three'},
+    ]
+
+    const loading = false;
+    const showComments = true;
+    if (loading) return <p>Loading...</p>
+
+    return (
+      <div className="container">
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+        {showComments && (
+            <div className='comments'>
+            <h3>Comments ({comments.length})</h3>  
+            <ul>
+                {comments.map( (comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+            </div>)
+            }
+      </div>
+    )
 }
 
 export default App;

0da4cbbfa2e857270ebe20f4007d054fe385fcb1
wlowry88 <william.c.lowry@gmail.com>
2022-04-09 18:09:01 -0400

    added use of prev state

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -5,10 +5,17 @@ function FeedbackItem() {
 
     const [rating, setRating] = useState(7)
     const [text, setText] = useState('This is an example of a feedback item')
+
+    const handleClick = () => {
+      setRating((prev) => {
+        return prev + 1;
+      });
+    }
   return (
     <div className='card'>
         <div className="num-display">{rating}</div>
         <div className="text-display">{text}</div>
+        <button onClick={handleClick}>Click</button>
     </div>
   )
 }

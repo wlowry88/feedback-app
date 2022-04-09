1ded8837d5abc9ba72e8690ff59176eb4ce4258b
wlowry88 <william.c.lowry@gmail.com>
2022-04-09 18:36:55 -0400

    some props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import {useState} from 'react'
+import FeedbackData from './data/FeedbackData';
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from './components/FeedbackList';
 
 function App(props) {
+    const [feedback, setFeedback] = useState(FeedbackData);
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackItem/>
+                <FeedbackList feedback={feedback}/>
             </div>
         </>
     )

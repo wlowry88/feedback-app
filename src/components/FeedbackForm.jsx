303ae986ab3287dd9978f0aa94353f84a7eb123e
wlowry88 <william.c.lowry@gmail.com>
2022-07-13 16:40:55 -0400

    progress on edit

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
 import Button from './Button'
 import RatingSelect from './RatingSelect'
-import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackForm () {
+
   const [text, setText] = useState('')
   const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
-  const {addFeedback} = useContext(FeedbackContext)
+  const { addFeedback, feedbackEdit } = useContext(FeedbackContext)
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+    }
+  }, [feedbackEdit])
 
   const handleTextChange = (e) => {
     if (text === '') {
@@ -41,27 +49,27 @@ function FeedbackForm () {
   }
 
   return (
-        <Card>
-            <form onSubmit={handleSubmit}>
-                <h2>How would you rate your service with us?</h2>
-                <RatingSelect select={(rating) => setRating(rating)}></RatingSelect>
-                <div className="input-group">
-                    <input
-                        onChange={handleTextChange}
-                        type="text"
-                        placeholder="Write a review"
-                        value={text}
-                    />
-                    <Button
-                        type="submit"
-                        version=""
-                        isDisabled={btnDisabled}>
-                        Send
-                    </Button>
-                </div>
-                {message && <div className="message">{message}</div>}
-            </form>
-        </Card>
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)}></RatingSelect>
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button
+            type="submit"
+            version=""
+            isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
   )
 }
 
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -50,7 +50,8 @@ export const FeedbackProvider = ({ children }) => {
     feedback,
     deleteFeedback,
     addFeedback,
-    editFeedback
+    editFeedback,
+    feedbackEdit,
   }}>
     {children}
   </FeedbackContext.Provider>
